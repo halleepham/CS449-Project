800a14b4813c25fe13ccac1c46c22df69606d65d
halleepham <113535116+halleepham@users.noreply.github.com>
2025-10-16 15:07:20 -0500

    Added game mode radio buttons

--- src/sprint1/product/SosGui.java
+++ src/sprint1/product/SosGui.java
@@ -2,24 +2,46 @@ package sprint1.product;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class SosGui extends Application {
+
+	private BorderPane layout;
+	private RadioButton rbSimpleGame, rbGeneralGame;
 	
 
 	@Override
 	public void start(Stage primaryStage) {
-		 
-		BorderPane borderPane = new BorderPane();
 		
-		Scene scene = new Scene(borderPane);
+		layout = new BorderPane();
+		Scene scene = new Scene(layout);
+		
+		buildSettingsPane();
 		
 		
 		primaryStage.setTitle("SOS Game");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
+	}
+	
+	public void buildSettingsPane() {
+		
+		ToggleGroup rbGameModeGroup  = new ToggleGroup();
+		rbSimpleGame = new RadioButton("Simple Game");
+		rbGeneralGame = new RadioButton("General Game");
+		
+		GridPane settingsPane = new GridPane();
+		settingsPane.add(rbSimpleGame, 0, 0);
+		settingsPane.add(rbGeneralGame, 1, 0);
+		
+		rbSimpleGame.setToggleGroup(rbGameModeGroup);
+		rbGeneralGame.setToggleGroup(rbGameModeGroup);
+		layout.setTop(settingsPane);
 		
 	}
 	
