748c2d3b87a746456c78b44285502cda0566abea
halleepham <113535116+halleepham@users.noreply.github.com>
2025-10-16 15:27:21 -0500

    Added board size label and input field

--- src/sprint1/product/SosGui.java
+++ src/sprint1/product/SosGui.java
@@ -1,8 +1,11 @@
 package sprint1.product;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -12,12 +15,14 @@ public class SosGui extends Application {
 
 	private BorderPane layout;
 	private RadioButton rbSimpleGame, rbGeneralGame;
+	private TextField txtBoardSize;
 	
 
 	@Override
 	public void start(Stage primaryStage) {
 		
 		layout = new BorderPane();
+		layout.setPadding(new Insets(10, 20, 10, 20));
 		Scene scene = new Scene(layout);
 		
 		buildSettingsPane();
@@ -31,19 +36,30 @@ public class SosGui extends Application {
 	
 	public void buildSettingsPane() {
 		
-		ToggleGroup rbGameModeGroup  = new ToggleGroup();
+		GridPane settingsPane = new GridPane();
+		settingsPane.setHgap(10);
+		
 		rbSimpleGame = new RadioButton("Simple Game");
 		rbGeneralGame = new RadioButton("General Game");
+		ToggleGroup rbGameModeGroup  = new ToggleGroup();
+		rbSimpleGame.setToggleGroup(rbGameModeGroup);
+		rbGeneralGame.setToggleGroup(rbGameModeGroup);
 		
-		GridPane settingsPane = new GridPane();
 		settingsPane.add(rbSimpleGame, 0, 0);
 		settingsPane.add(rbGeneralGame, 1, 0);
 		
-		rbSimpleGame.setToggleGroup(rbGameModeGroup);
-		rbGeneralGame.setToggleGroup(rbGameModeGroup);
+		Label lblBoardSize = new Label("Board size: ");
+		settingsPane.add(lblBoardSize, 2, 0);
+		
+		txtBoardSize = new TextField();
+		txtBoardSize.setMaxWidth(20);
+		settingsPane.add(txtBoardSize, 3, 0);
+		
 		layout.setTop(settingsPane);
 		
+		
 	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
