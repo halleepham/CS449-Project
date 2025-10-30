921ece24942c0681e1bad37fd0f4a53692c91bfd
halleepham <113535116+halleepham@users.noreply.github.com>
2025-10-30 14:49:23 -0500

    Changed checkOFormed() and checkSFormed() to add sosLines when sos formed

--- src/sprint3/product/SosGame.java
+++ src/sprint3/product/SosGame.java
@@ -1,5 +1,6 @@
 package sprint3.product;
 
+import java.util.ArrayList;
 
 public abstract class SosGame {
 	
@@ -22,6 +23,7 @@ public abstract class SosGame {
 	protected GameState currentGameState;
   protected int bluePoints;
   protected int redPoints;
+  protected ArrayList<SosLine> sosLines;
 	
 	public abstract void makeMove(int row, int col) throws Exception;
 	
@@ -36,6 +38,7 @@ public abstract class SosGame {
 		blueMove = 'S';
 		redMove = 'S';
 		turn = PlayerTurn.BLUE;
+		sosLines = new ArrayList<SosLine>();
 	}
 	
 	public void setUpNewBoard(int size) {
@@ -130,21 +133,25 @@ public abstract class SosGame {
 		if (col > 0 && col < totalColumns - 1 
 				&& grid[row][col - 1] == Cell.S && grid[row][col + 1] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row, col - 1, row, col + 1, turn));
 		}
 		// Vertical
 		if (row > 0 && row < totalRows - 1 
 				&& grid[row - 1][col] == Cell.S && grid[row + 1][col] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row - 1, col, row + 1, col, turn));
 		}
 		// Diagonal (top left to bottom right)
 		if (row - 1 >= 0 && col - 1 >= 0 && row + 1 < totalRows && col + 1 < totalColumns
 				&& grid[row - 1][col - 1] == Cell.S && grid[row + 1][col + 1] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row - 1, col - 1, row + 1, col + 1, turn));
 		}
 		// Diagonal (top right to bottom left)
 		if (row - 1 >= 0 && col + 1 < totalColumns && row + 1 < totalRows && col - 1 >= 0
 				&& grid[row - 1][col + 1] == Cell.S && grid[row + 1][col - 1] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row - 1, col + 1, row + 1, col - 1, turn));
 		}
 		return numSosFormed;
 	}
@@ -155,41 +162,49 @@ public abstract class SosGame {
 		if (col + 2 < totalColumns 
 				&& grid[row][col + 1] == Cell.O && grid[row][col + 2] == Cell.S) {
 			numSosFormed++;
+			sosLines.add(new SosLine(row, col, row, col + 2, turn));
 		}
 		// Horizontal left
 		if (col - 2 >= 0 
 				&& grid[row][col - 1] == Cell.O && grid[row][col - 2] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row, col, row, col - 2, turn));
 		}
 		// Vertical down
 		if (row + 2 < totalRows 
 				&& grid[row + 1][col] == Cell.O && grid[row + 2][col] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row, col, row + 2, col, turn));
 		}
 		// Vertical up
 		if (row - 2 >= 0 
 				&& grid[row - 1][col] == Cell.O && grid[row - 2][col] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row, col, row - 2, col, turn));
 		}
 		// Diagonal (top left to bottom right)
 		if (row + 2 < totalRows && col + 2 < totalColumns 
 				&& grid[row + 1][col + 1] == Cell.O && grid[row + 2][col + 2] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row, col, row + 2, col + 2, turn));
 		}
 		// Diagonal (top right to bottom left)
 		if (row + 2 < totalRows && col - 2 >= 0 
 				&& grid[row + 1][col - 1] == Cell.O && grid[row + 2][col - 2] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row, col, row + 2, col - 2, turn));
 		}
 		// Diagonal (bottom left to top right)
 		if (row - 2 >= 0 && col + 2 < totalColumns 
 				&& grid[row - 1][col + 1] == Cell.O && grid[row - 2][col + 2] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row, col, row - 2, col + 2, turn));
 		}
 		// Diagonal (bottom right to top left)
 		if (row - 2 >= 0 && col - 2 >= 0 
 				&& grid[row - 1][col - 1] == Cell.O && grid[row - 2][col - 2] == Cell.S) {
 		  numSosFormed++;
+		  sosLines.add(new SosLine(row, col, row - 2, col - 2, turn));
 		}
 		return numSosFormed;
 	}
