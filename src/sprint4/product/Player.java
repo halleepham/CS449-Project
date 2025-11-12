90c6749747a6cf4ca60f38d09eb9f6ca5092c2f1
halleepham <113535116+halleepham@users.noreply.github.com>
2025-11-11 21:08:43 -0600

    Added abstract Player class

--- src/sprint4/product/Player.java
+++ src/sprint4/product/Player.java
@@ -1,5 +1,26 @@
 package sprint4.product;
 
-public class Player {
-
+public abstract class Player {
+  protected char move;
+  protected int points;
+  
+  public Player() {
+    this.points = 0;
+  }
+  
+  public char getMove() {
+    return move;
+  }
+  
+  public void setMove(char move) {
+    this.move = move;
+  }
+  
+  public int getPoints() {
+    return points;
+  }
+  
+  public void setPoints(int points) {
+    this.points = points;
+  }
 }
