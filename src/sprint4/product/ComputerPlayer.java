23a4606f014a5a200e62515b049081ee4a78ace2
halleepham <113535116+halleepham@users.noreply.github.com>
2025-11-16 15:27:40 -0600

    Implemented pickSosMove() for computer player

--- src/sprint4/product/ComputerPlayer.java
+++ src/sprint4/product/ComputerPlayer.java
@@ -1,15 +1,36 @@
 package sprint4.product;
 
+import sprint4.product.SosGame.Cell;
+
 public class ComputerPlayer extends Player {
 
   @Override
   public int[] selectMove(SosGame game) {
-    // TODO Auto-generated method stub
-    return pickFirstEmptyMove(game);
+    int[] move = pickSosMove(game);
+    if (move == null) {
+      move = pickSafeMove(game);
+      if (move == null) {
+        move = pickFirstEmptyMove(game);
+      }
+    }
+    return move;
   }
 
   private int[] pickSosMove(SosGame game) {
-    // TODO: implement logic after getting player logic working
+    //TODO: Change to pick the move that makes the most soses
+    for (int row = 0; row < game.getTotalRows(); row++) {
+      for (int col = 0; col < game.getTotalColumns(); col++) {
+        if (game.getCell(row, col) == Cell.EMPTY) {
+          if (game.checkOFormed(row, col).size() > 0) {
+            this.setMove('O');
+            return new int[] {row, col};
+          } else if (game.checkSFormed(row, col).size() > 0) {
+            this.setMove('S');
+            return new int[] {row, col};
+          }
+        }
+      }
+    }
     return null;
   }
   
